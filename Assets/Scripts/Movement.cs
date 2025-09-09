d8a84984fc9ff8628ec75ab60fc9c07f793246d4
Zniver4 <jonathan4zacos@gmail.com>
2025-09-09 10:19:22 -0600

    rotation and linerenderer

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,13 +2,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5f;
+    public float rotationSpeed = 120f;
+    public LineRenderer laserRenderer;
+    public Transform laserOrigin;
+    public float laserLength = 10f;
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
+        UpdateLaser();
 
-        Vector3 direction = new Vector3(Vertical, 0, horizontal);
-        transform.Translate(direction * speed * Time.deltaTime);
+        bool isRotatingOnly = Input.GetButton("LB");
+
+        if (!isRotatingOnly)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 direction = new Vector3(vertical, 0, horizontal);
+            Vector3 moveDirection = transform.TransformDirection(direction);
+            transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        }
+
+        float rotateInput = Input.GetAxis("Mouse X"); 
+        transform.Rotate(Vector3.up, rotateInput * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("RB"))
+        {
+            Shoot();
+        }
+    }
+
+    void UpdateLaser()
+    {
+        if (laserRenderer != null && laserOrigin != null)
+        {
+            laserRenderer.SetPosition(0, laserOrigin.position);
+            laserRenderer.SetPosition(1, laserOrigin.position + laserOrigin.forward * laserLength);
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Disparo realizado");
     }
 }
