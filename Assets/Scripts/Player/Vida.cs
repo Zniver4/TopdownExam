b8c1ee254a6286f879b50e0b6cb36a39d9b7cd79
Zniver4 <jonathan4zacos@gmail.com>
2025-09-10 12:36:11 -0600

    plus vida

--- Assets/Scripts/Player/Vida.cs
+++ Assets/Scripts/Player/Vida.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
-
+using TMPro;
 public class Vida : MonoBehaviour
 {
     public float health = 100f;
     public GameObject gameOverCanvas;
+    public TMP_Text healthText;
+    void Start()
+    {
+        UpdateHealthUI();
+
+        if (gameOverCanvas != null)
+            gameOverCanvas.SetActive(false);
+    }
     public void TakeDamage(float damage)
     {
         health -= damage;
         Debug.Log($"Jugador recibió {damage} de daño. Vida restante: {health}");
+
+        UpdateHealthUI();
 
         if (health <= 0f)
         {
@@ -25,5 +35,11 @@ public class Vida : MonoBehaviour
         {
             gameOverCanvas.SetActive(true); 
         }
+    }
+
+    void UpdateHealthUI()
+    {
+        if (healthText != null)
+            healthText.text = $"Vida: {Mathf.Max(0, Mathf.RoundToInt(health))}";
     }
 }
